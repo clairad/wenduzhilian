f8ad816bd75e2d04efd42deb209ebabf26fe6178
Clairad <623131118@qq.com>
2024-02-02 09:33:44 +0800

    2月2日 版本同步

--- CZ-1C++3班/C语言day3-3月25日-选择、循环结构/3月25日代码/阿姆斯特朗数/main.c
+++ CZ-1C++3班/C语言day3-3月25日-选择、循环结构/3月25日代码/阿姆斯特朗数/main.c
@@ -16,14 +16,14 @@ int main()
 			k++;
 		}
 
-			for (i = n; i; i /= 10)
-			{
-				sum += pow(i % 10, k);
-			}
-			if (sum == n)
-			{
-				printf("%d\n", sum);
-			}
+		for (i = n; i; i /= 10)
+		{
+			sum += pow(i % 10, k);
+		}
+		if (sum == n)
+		{
+			printf("%d\n", sum);
+		}
 		sum = 0;
 	}
 	return 0;
